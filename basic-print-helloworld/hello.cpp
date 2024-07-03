276f73d296ce337d232b7b3915e8386d5638bf26
Muhamad Marifat Al Hanif <marifatalhanif@gmail.com>
2024-07-04 06:51:41 +0700

    feat: HTTP RESTful API in C

--- basic-print-helloworld/hello.cpp
+++ basic-print-helloworld/hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+   // Print "Hello, World!" to the terminal
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
