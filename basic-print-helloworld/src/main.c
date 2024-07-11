12f1d5c9ddd64331f0a87ee8873a65988b7a8b22
Muhamad Marifat Al Hanif <marifatalhanif@gmail.com>
2024-07-12 05:41:02 +0700

    Working with files

--- basic-print-helloworld/src/main.c
+++ basic-print-helloworld/src/main.c
@@ -1,7 +1,26 @@
-#include "callme.h"
+#include <stdio.h>
 
 int main()
 {
-    callthis();
+    FILE *fptr;
+    char ch;
+
+    fptr = fopen("file.txt", "r");
+
+    if (NULL == fptr) {
+        printf("File not found\n");
+        return 1;
+    }
+
+    printf("content of this file are \n");
+
+    do {
+        ch = fgetc(fptr);
+        printf("%c", ch);
+    } while (ch != EOF);
+
+    fclose(fptr);
+    getchar();
     return 0;
 }
+
