9a0c914ae67add91d48e2967a9ad0cba061fd324
Hanif <muhamad.marifat@pactindo.com>
2024-07-15 19:11:42 +0700

    Some rando changes

--- basic-print-helloworld/src/main.c
+++ basic-print-helloworld/src/main.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
     int *ptr;
+    ptr = (int *) malloc(sizeof(int));
+    printf("Address of the pointer: %p\n", ptr);
+    printf("Value of the pointer: %d\n", *ptr);
+    *ptr = 10;
+
+    free(ptr);
+    getchar();
 }
